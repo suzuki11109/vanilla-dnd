f4ee4dbe2f2fd877dab305dad86413d72c058436
Suzuki Aki <suzuki11109@gmail.com>
2018-10-19 15:11:50 +0700

    update title, remove comments

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 let dropArea = document.getElementById('drop-area');
 
-/* dropArea.addEventListener('dragenter', handlerFunction, false);
- * dropArea.addEventListener('dragleave', handlerFunction, false);
- * dropArea.addEventListener('dragover', handlerFunction, false);
- * dropArea.addEventListener('drop', handlerFunction, false);
- *  */
 ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
   dropArea.addEventListener(eventName, preventDefaults, false)
 })
