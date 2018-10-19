20a879d6d76aa8b28e6bc0cce4a275c6cafe8f25
Suzuki Aki <suzuki11109@gmail.com>
2018-10-19 14:54:11 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+let dropArea = document.getElementById('drop-area');
+
+/* dropArea.addEventListener('dragenter', handlerFunction, false);
+ * dropArea.addEventListener('dragleave', handlerFunction, false);
+ * dropArea.addEventListener('dragover', handlerFunction, false);
+ * dropArea.addEventListener('drop', handlerFunction, false);
+ *  */
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+  dropArea.addEventListener(eventName, preventDefaults, false)
+})
+
+function preventDefaults (e) {
+  e.preventDefault()
+  e.stopPropagation()
+}
+
+['dropenter', 'dragover'].forEach(eventName => {
+  dropArea.addEventListener(eventName, highlight, false);
+});
+
+['dragleave', 'drop'].forEach(eventName => {
+  dropArea.addEventListener(eventName, unhighlight, false);
+});
+
+function highlight(e) {
+  dropArea.classList.add('highlight');
+}
+
+function unhighlight(e) {
+  dropArea.classList.remove('highlight');
+}
+
+dropArea.addEventListener('drop', handleDrop, false);
+function handleDrop(e) {
+  let dt = e.dataTransfer;
+  let files = dt.files;
+
+  handleFiles(files);
+}
+
+
+function handleFiles(files) {
+  ([...files]).forEach(uploadFile);
+  ([...files]).forEach(previewFile);
+}
+
+function uploadFile(file) {
+  console.log(file);
+}
+
+function previewFile(file) {
+  let reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onloadend = function() {
+    let img = document.createElement('img');
+    img.src = reader.result;
+    document.getElementById('gallery').appendChild(img);
+  }
+}
